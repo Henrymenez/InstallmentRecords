36aaaa5d4cec175b5e7ee088b7d7a8f21ca33417
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2022-11-24 23:46:56 +0100

    added more functionalities to it

--- InstallmentRecords/InstallmentRecords.cs
+++ InstallmentRecords/InstallmentRecords.cs
@@ -4,15 +4,15 @@
     {
         private static DateTime _DayOfPurchase;
 
-        //calculate for daily installment plan
+      
         public static void output(string purchaseDate, decimal installmentAmount,decimal totalAmount,string productName,string customerName)
         {
-        
-        
-            Console.WriteLine($"Here Here Purchased on {purchaseDate}," +
+
+
+            Console.WriteLine($"Here Purchased on {purchaseDate}," +
                 $" Installment  Amount ₦ {installmentAmount}," +
                 $"Total Amount Recieved ₦ {totalAmount} " +
-                $" For Product {totalAmount} by Customer {totalAmount}");
+                $" For Product {productName} by Customer {customerName}\n");
         }
 
         public static void DailyInstallmentPlan(Customer customer)
@@ -40,7 +40,7 @@
 
         }
 
-        //calculate for daily installment plan
+        
         public static void WeeklyInstallmentPlan(Customer customer)
         {
             try
@@ -67,7 +67,7 @@
 
         }
 
-        //calculate for daily installment plan
+        
         public static void BiWeeklyInstallmentPlan(Customer customer)
         {
             try
@@ -94,8 +94,83 @@
 
         }
 
+        public static void MonthlyInstallmentPlan(Customer customer)
+        {
+            try
+            {
+                decimal InstallmentPercentage = (decimal)(10d / 100);
+                _DayOfPurchase = DateTime.Now;
+                decimal InstallmentAmount = customer.GetProductAmount() * InstallmentPercentage;
+
+                for (decimal i = InstallmentAmount; i <= customer.GetProductAmount(); i += InstallmentAmount)
+                {
+
+                    output(_DayOfPurchase.ToLongDateString(), InstallmentAmount,
+                         customer.GetProductAmount(), customer.GetProductName(),
+                         customer.GetCustumerName()
+                         );
+                    _DayOfPurchase = _DayOfPurchase.AddMonths(1);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
 
+        }
+
+        public static void Six_MonthlyInstallmentPlan(Customer customer)
+        {
+            try
+            {
+                decimal InstallmentPercentage = (decimal)(15d / 100);
+                _DayOfPurchase = DateTime.Now;
+                decimal InstallmentAmount = customer.GetProductAmount() * InstallmentPercentage;
+
+                for (decimal i = InstallmentAmount; i <= customer.GetProductAmount(); i += InstallmentAmount)
+                {
+
+                    output(_DayOfPurchase.ToLongDateString(), InstallmentAmount,
+                         customer.GetProductAmount(), customer.GetProductName(),
+                         customer.GetCustumerName()
+                         );
+                    _DayOfPurchase = _DayOfPurchase.AddMonths(6);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+        public static void YearlyInstallmentPlan(Customer customer)
+        {
+            try
+            {
+                decimal InstallmentPercentage = (decimal)(50d / 100);
+                _DayOfPurchase = DateTime.Now;
+                decimal InstallmentAmount = customer.GetProductAmount() * InstallmentPercentage;
+
+                for (decimal i = InstallmentAmount; i <= customer.GetProductAmount(); i += InstallmentAmount)
+                {
+
+                    output(_DayOfPurchase.ToLongDateString(), InstallmentAmount,
+                         customer.GetProductAmount(), customer.GetProductName(),
+                         customer.GetCustumerName()
+                         );
+                    _DayOfPurchase = _DayOfPurchase.AddYears(1);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
     }
 
 
@@ -115,23 +190,46 @@
                 decimal ProductAmount = Decimal.Parse(Console.ReadLine());
 
                 Console.WriteLine("Select an Installment Payment Plan ");
-                Console.WriteLine("2% installmental Payment with Daily Plan, Type 1");
-                Console.WriteLine("5% installmental Payment with Weekly Plan , Type 2");
-                Console.WriteLine("7% installmental Payment with Bi-Weekly Plan , Type 3");
-                Console.WriteLine("10% installmental Payment with Monthly Plan , Type 4");
-                Console.WriteLine("15% installmental Payment with Six-Month Plan , Type 5");
-                Console.WriteLine("50% installmental Payment with Yearly , Type 6");
+                Console.WriteLine("1. 2% installmental Payment with Daily Plan");
+                Console.WriteLine("2. 5% installmental Payment with Weekly Plan");
+                Console.WriteLine("3. 7% installmental Payment with Bi-Weekly Plan");
+                Console.WriteLine("4. 10% installmental Payment with Monthly Plan");
+                Console.WriteLine("5. 15% installmental Payment with Six-Month Plan");
+                Console.WriteLine("6. 50% installmental Payment with Yearly");
                 string? plan = Console.ReadLine();
 
                 Customer customer = new Customer(CustomerName, ProductName, ProductAmount);
+                    if (plan == "1")
+                    {
+                        InstallmentRecords.DailyInstallmentPlan(customer);
+                    }
+                    else if (plan == "2")
+                    {
+                        InstallmentRecords.WeeklyInstallmentPlan(customer);
+                    }
+                    else if (plan == "3")
+                    {
+                        InstallmentRecords.BiWeeklyInstallmentPlan(customer);
 
-                if (plan == "1")
-                {
-                    InstallmentRecords.DailyInstallmentPlan(customer);
-                }else if (plan == "2")
-                {
-                    InstallmentRecords.WeeklyInstallmentPlan(customer);
-                }
+                    }
+                    else if (plan == "4")
+                    {
+                        InstallmentRecords.MonthlyInstallmentPlan(customer);
+                    }
+                    else if (plan == "5")
+                    {
+                        InstallmentRecords.Six_MonthlyInstallmentPlan(customer);
+                    }
+                    else if (plan == "6")
+                    {
+                        InstallmentRecords.YearlyInstallmentPlan(customer);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Choose a valid option");
+                    Console.ReadLine();
+                    }
+                
             }catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
